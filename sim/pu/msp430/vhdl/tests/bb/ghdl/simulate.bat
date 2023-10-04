bcded5fa88e11a677c5a7b06287287a7433edc97
PacoReinaCampo <frareicam@gmail.com>
2023-10-04 15:43:19 +0400

    Working in Peripheral-DBG 🔑

--- sim/pu/msp430/vhdl/tests/bb/ghdl/simulate.bat
+++ sim/pu/msp430/vhdl/tests/bb/ghdl/simulate.bat
@@ -44,16 +44,7 @@
 
 @echo off
 call ../../../../../../../settings64_ghdl.bat
-
-ghdl -a --std=08 ../../../../../../../rtl/pu/msp430/vhdl/code/pkg/bb/peripheral_dbg_pu_msp430_pkg.vhd
-
-ghdl -a --std=08 ../../../../../../../rtl/pu/msp430/vhdl/code/peripheral/bb/main/peripheral_dbg_pu_msp430.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/msp430/vhdl/code/peripheral/bb/omsp/peripheral_dbg_pu_msp430_i2c.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/msp430/vhdl/code/peripheral/bb/omsp/peripheral_dbg_pu_msp430_uart.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/msp430/vhdl/code/peripheral/bb/fuse/peripheral_dbg_pu_msp430_sync_cell.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/msp430/vhdl/code/peripheral/bb/omsp/peripheral_dbg_pu_msp430_hwbrk.vhd
-
-ghdl -a --std=08 ../../../../../../../bench/pu/msp430/vhdl/code/tests/bb/peripheral_dbg_testbench.vhd
+sh system.s
 
 ghdl -e --std=08 peripheral_dbg_testbench
 ghdl -r --std=08 peripheral_dbg_testbench --ieee-asserts=disable-at-0 --vcd=peripheral_dbg_testbench.vcd --wave=system.ghw --stop-time=1ms
--- sim/pu/riscv/vhdl/tests/wb/ghdl/simulate.bat
+++ sim/pu/riscv/vhdl/tests/wb/ghdl/simulate.bat
@@ -44,26 +44,7 @@
 
 @echo off
 call ../../../../../../../settings64_ghdl.bat
-
-ghdl -a --std=08 ../../../../../../../rtl/pu/riscv/vhdl/code/pkg/core/peripheral_dbg_pu_riscv_pkg.vhd
-
-ghdl -a --std=08 ../../../../../../../rtl/pu/riscv/vhdl/code/peripheral/wb/peripheral_dbg_pu_riscv_jsp_biu_wb.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/riscv/vhdl/code/peripheral/wb/peripheral_dbg_pu_riscv_jsp_module_wb.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/riscv/vhdl/code/peripheral/wb/peripheral_dbg_pu_riscv_top_wb.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/riscv/vhdl/code/peripheral/wb/peripheral_dbg_pu_riscv_biu_wb.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/riscv/vhdl/code/peripheral/wb/peripheral_dbg_pu_riscv_module_wb.vhd
-
-ghdl -a --std=08 ../../../../../../../rtl/pu/riscv/vhdl/code/core/peripheral_dbg_pu_riscv_bus_module_core.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/riscv/vhdl/code/core/peripheral_dbg_pu_riscv_bytefifo.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/riscv/vhdl/code/core/peripheral_dbg_pu_riscv_crc32.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/riscv/vhdl/code/core/peripheral_dbg_pu_riscv_jsp_module_core.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/riscv/vhdl/code/core/peripheral_dbg_pu_riscv_biu.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/riscv/vhdl/code/core/peripheral_dbg_pu_riscv_module.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/riscv/vhdl/code/core/peripheral_dbg_pu_riscv_status_reg.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/riscv/vhdl/code/core/peripheral_dbg_pu_riscv_syncflop.vhd
-ghdl -a --std=08 ../../../../../../../rtl/pu/riscv/vhdl/code/core/peripheral_dbg_pu_riscv_syncreg.vhd
-
-ghdl -a --std=08 ../../../../../../../bench/pu/riscv/vhdl/code/tests/wb/peripheral_dbg_testbench.vhd
+sh system.s
 
 ghdl -e --std=08 peripheral_dbg_testbench
 ghdl -r --std=08 peripheral_dbg_testbench --ieee-asserts=disable-at-0 --vcd=peripheral_dbg_testbench.vcd --wave=system.ghw --stop-time=1ms
